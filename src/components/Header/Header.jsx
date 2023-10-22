77b74366042d70d3dda648b080fb0b293efe3c18
sarvaralimovq <sarvaralimovq@gmail.com>
2023-10-22 13:14:06 +0500

    sa

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.scss'
+function Header() {
+  return (
+    <div className='Header'>
+        <div className="container">
+            <nav>
+                salom
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
